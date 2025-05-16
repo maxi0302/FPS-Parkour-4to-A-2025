e80941569150a683bfeb9d0847ff83c0befc1c7f
jeronimosanchezperuga <70541244+jeronimosanchezperuga@users.noreply.github.com>
2025-05-16 09:05:07 -0300

    First parkour y respawn on fall

---
+++ Assets/RestartSceneOnFall.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RestartSceneOnFall : MonoBehaviour
+{
+    public float deathFallHeight = -15;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.y < deathFallHeight)
+        {
+            Debug.Log("Respawn");
+            SceneManager.LoadScene("FPS Parkour");
+        }
+    }
+}
